4c5eb5e7f117161546bbdb20b5796dce1dc4a48b
Aleksey <rusal@bk.ru>
2022-09-16 13:59:31 +0300

    fix

--- PR02/MSRKPO.cpp
+++ PR02/MSRKPO.cpp
@@ -1,7 +1,35 @@
 ﻿#include <iostream>
+using namespace std;
+
+struct student {
+    string surName;
+    string name;
+    string middleName;
+    int math;
+    int phys;
+    int draw;
+    int chem;
+    int sopromat;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //srand(8954);
+    //int variant = rand() % 10;
+    //cout << variant;
+    // variant 5
+    //Воспользовавшись записью СТУДЕНТ из варианта №1, 
+    // определите, сколько студентов имеют 
+    //средний балл от 4 до 5.
+    //Опишите запись СТУДЕНТ и поместите в нее следующую информацию: Ф.И.О., 
+    // оценки (математика, физика, черчение, химия, сопромат).
+    setlocale(LC_ALL, "rus");
+    int n = 5;
+    student  st = {};
+    for (int i = 0; i < n; i++) {
+        st[]
+    }
+
+
 }
 
